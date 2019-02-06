1b08fcf8896b3a7f4b382475616df586932b1003
Jameson Nelson <nelson_jameson@yahoo.com>
2019-02-06 10:17:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function takeANumber(current, name){
 function nowServing(katzDeliLine){
   let message;
   let firstPerson = katzDeliLine.forEach(function(value){
-    message = `Currently serving ${vaule}.`;
+    message = `Currently serving ${value}.`;
     return message;
   });
   
