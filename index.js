ecfaba60f368f3033b93a91143964c2d250f3b7b
Jameson Nelson <nelson_jameson@yahoo.com>
2019-02-06 10:48:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function takeANumber(current, name){
 function nowServing(katzDeliLine){
   let message;
   if(katzDeliLine.lenth ===0){
-   let message = 'There is nobody waiting to be served!';
+   message = 'There is nobody waiting to be served!';
    return message;
   }
 }
