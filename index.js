b53c80f374d6d4a512cb28415a1dab40316f098f
Jameson Nelson <nelson_jameson@yahoo.com>
2019-02-06 10:11:59 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,26 @@
+//takeANumber function.
+function takeANumber(current, name){
+  let newPerson = current.push(name);
+  let position = current.indexOf(name) +1;
+  let message = `Welcome, ${name}. You are number ${position} in line.`;
+  
+  return message;
+}
+
+
+//nowServing function.
+
+//const deliLine = ["Steven", "Blake", "Avi"];
+
+function nowServing(katzDeliLine){
+  let message;
+  let currentPerson = katzDeliLine.forEach(function(value){ 
+    if(katzDeliLine.length === 0){
+      message = "There is nobody waiting to be served!";
+    }else{
+      message = `Currently serving ${value}.`;
+      katzDeliLine.shift();
+    }
+    return message;
+  });
+}
